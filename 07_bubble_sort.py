fa3a9f1f50ed982adbc137c75b66106606d03c3f
Ronie Corinto <roniecortinto@gmail.com>
2026-03-19 09:53:06 -0300

    (18/03) Término de Bubble Sort e início de Selection Sort

--- 07_bubble_sort.py
+++ 07_bubble_sort.py
@@ -1,53 +1,11 @@
-<<<<<<< HEAD
-=======
 # Variáveis globais de estatística
 comps = trocas = passd = None
 
->>>>>>> 44574ab7b3e546fb7b400d3193920d38dd0b0215
 def bubble_sort(lista):
     """
     ALGORITMO DE ORDENAÇÃO BUBBLE SORT
     Percorre a lista a ser ordenada em sucessivas passadas,
     trocando entre si dois elementos adjacentes sempre que
-<<<<<<< HEAD
-    o SEGUNDO for MENOR do que o PRIMEIRO. Efetua tantas 
-    passadas quanto necessárias, até que, na última passada,
-    nenhuma troca tenha sido feita.
-    """
-
-    # Loop eterno: não sabemos com antecedência quantas
-    # passadas serão necessárias para concluir a ordenação 
-    while True:
-         # Variável que controla se houve ou não troca durante
-         # a passada
-         trocou = False
-
-         # Percorre a lista, do primeiro ao PENÚLTIMO elemento,
-         # com acesso a cada posição
-         for pos in range(len(lista) - 1):
-            # Se o valor que está à frente na lista (pos + 1)
-            # for MENOR do que o seu antecessor (pos), efetuamos
-            # uma troca entre eles
-            if lista[pos + 1] < lista[pos]:
-                # Faz a troca (swap) direita
-                lista[pos + 1], lista[pos] = lista[pos], lista[pos + 1]
-                # Assinala que houve troca na passada
-                trocou = True
-
-    # <~ CUIDADO COM A IDENTAÇÃO AQUI!
-    # Se não houve troca na passada (trocou ainda tem o valor False),
-    # a lista está ordenada e interrompemos o loop while
-    if not trocou: break
-
-#####################################################################################
-
-# Caso médio
-# nums = [7, 0, 8, 1,=]
-
-#####################################################################################
-
-# TESTE COM 1M NOMES
-=======
     o SEGUNDO for MENOR do que o PRIMEIRO. Efetua tantas
     passadas quanto necessárias, até que, na última passada,
     nenhuma troca tenha sido feita.
@@ -112,33 +70,19 @@ print(f"Comparações: {comps}; trocas: {trocas}; passadas: {passd}")
 ######################################################################
 
 # TESTE COM 1M+ NOMES
->>>>>>> 44574ab7b3e546fb7b400d3193920d38dd0b0215
 
 from time import time
 
 import sys
-<<<<<<< HEAD
-sys.dont_write_bytecode = True          # Destiva a criação do cache
-
-from data.nomes_desord import nomes_desord
-
-hora_ini = time()
-bubble_sort (nomes)
-=======
 sys.dont_write_bytecode = True      # Desativa a criação do cache
 
 from data.nomes_desord import nomes
 
 hora_ini = time()
 bubble_sort(nomes)
->>>>>>> 44574ab7b3e546fb7b400d3193920d38dd0b0215
 hora_fim = time()
 
 print(nomes)
 
 print(f"Comparações: {comps}; trocas: {trocas}; passadas: {passd}")
-<<<<<<< HEAD
 print(f"Tempo gasto: {(hora_fim - hora_ini) * 1000}ms.\n")
-=======
-print(f"Tempo gasto: {(hora_fim - hora_ini) * 1000}ms.\n")
->>>>>>> 44574ab7b3e546fb7b400d3193920d38dd0b0215
